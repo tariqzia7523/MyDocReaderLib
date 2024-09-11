1dc1d0f1bfedd208b8243a68e45cc872c51ea201
Muhammad Tariq Zia <57493857+tariqzia7523@users.noreply.github.com>
2024-09-11 13:40:33 +0500

    commit

--- app/src/main/java/com/all/doc/MainActivity.kt
+++ app/src/main/java/com/all/doc/MainActivity.kt
@@ -18,6 +18,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.all.doc.databinding.ActivityMainBinding
 import com.xls.bbbbb.office.FileListUtils
 import com.xls.bbbbb.office.FileModel
+import com.xls.bbbbb.office.OpenFileActivity
+import com.xls.bbbbb.office.constant.MainConstant
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -34,11 +36,14 @@ class MainActivity : AppCompatActivity() {
         binding.allList.layoutManager = LinearLayoutManager(this)
         adapter = AllFilesAdapter(this@MainActivity,filesList,object : AllFilesAdapter.OnFileSelected {
             override fun onFileSelected(fileModel: FileModel) {
+//                val intent = Intent()
+//                intent.setClass(this@MainActivity, FileViewerActivity::class.java)
+//                intent.putExtra("file_path", fileModel.filePath)
+//                startActivityForResult(intent, RESULT_FIRST_USER)
                 val intent = Intent()
-                intent.setClass(this@MainActivity, FileViewerActivity::class.java)
-                intent.putExtra("file_path", fileModel.filePath)
+                intent.setClass(this@MainActivity, OpenFileActivity::class.java)
+                intent.putExtra(MainConstant.INTENT_FILED_FILE_PATH, fileModel.filePath)
                 startActivityForResult(intent, RESULT_FIRST_USER)
-
 
             }
         })
@@ -75,6 +80,12 @@ class MainActivity : AppCompatActivity() {
             }
             try{
                 filesList.addAll(FileListUtils.getExternalFileList(this@MainActivity, MimeTypeMap.getSingleton().getMimeTypeFromExtension("xls")!!))
+            }catch (e : Exception){
+                e.printStackTrace()
+            }
+
+            try{
+                filesList.addAll(FileListUtils.getExternalFileList(this@MainActivity, MimeTypeMap.getSingleton().getMimeTypeFromExtension("pdf")!!))
             }catch (e : Exception){
                 e.printStackTrace()
             }
